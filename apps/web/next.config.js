8c8b3d94d2a5c1f8cc7b61573d7500928e3a62f2
zefparis <ben.barere@gmail.com>
2026-03-27 08:02:46 +0100

    f

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    }
+    return config
   },
 }
 
